1d0a024f37e36946c0f12ad6e3e2aed6220b4112
unknown <jose.correa-r@labinfo.is.escuelaing.edu.co>
2024-10-24 17:30:55 -0500

    sonarcloud

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,50 +1,46 @@
 var app = (function () {
-
-    var topic = "0";
-
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
+    let topic = "0"; // Cambiado a let
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
     }
-
-    class Polygon{
-        constructor(points){
+    class Polygon {
+        constructor(points) {
             this.points = points;
         }
     }
-    
-    var stompClient = null;
-    var canvas
 
-    var addPointToCanvas = function (point) {        
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
+    let stompClient = null; // Cambiado a let
+    let canvas; // Cambiado a let
+
+    let addPointToCanvas = function (point) {
+        let canvas = document.getElementById("canvas"); // Cambiado a let
+        let ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
-    
-    var getMousePosition = function (evt) {
+
+    let getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
-        var rect = canvas.getBoundingClientRect();
+        let rect = canvas.getBoundingClientRect(); // Cambiado a let
         return {
             x: evt.clientX - rect.left,
             y: evt.clientY - rect.top
         };
     };
 
-    var drawNewPolygon = function(polygon){
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
+    let drawNewPolygon = function (polygon) {
+        let canvas = document.getElementById('canvas'); // Cambiado a let
+        let ctx = canvas.getContext('2d');
         ctx.beginPath();
-        ctx.moveTo(polygon.points[0].x, polygon.points[0].y)
+        ctx.moveTo(polygon.points[0].x, polygon.points[0].y);
         ctx.fillStyle = '#00FF9B';
-        console.log("Longitud: " +  polygon.points.length);
-        for (var i = 1; i < polygon.points.length; i++) {
-            var point = polygon.points[i];
+        console.log("Longitud: " + polygon.points.length);
+        for (let i = 1; i < polygon.points.length; i++) { // Cambiado a let
+            let point = polygon.points[i]; // Cambiado a let
             ctx.lineTo(point.x, point.y);
         }
         ctx.lineTo(polygon.points[0].x, polygon.points[0].y);
@@ -52,82 +48,58 @@ var app = (function () {
         ctx.fill();
     }
 
-
-    var connectAndSubscribe = function (topic) {
+    let connectAndSubscribe = function (topic) {
         console.info('Connecting to WS...');
-        var socket = new SockJS('/stompendpoint');
+        let socket = new SockJS('/stompendpoint'); // Cambiado a let
         stompClient = Stomp.over(socket);
-        
-        //subscribe to /topic/TOPICXX when connections succeed
+
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic'+topic, function (eventbody) {
-                // para manejar eventos recibidos desde el servidor
-                var newPoint = JSON.parse(eventbody.body);
-                // var x = newPoint.x;
-                // var y = newPoint.y;
-                // //alert('Nuevo punto recibido - X: ' + x + ', Y: ' + y);
-                if(topic.includes("newpoint")){
+            stompClient.subscribe('/topic' + topic, function (eventbody) {
+                let newPoint = JSON.parse(eventbody.body); // Cambiado a let
+                if (topic.includes("newpoint")) {
                     addPointToCanvas(newPoint);
-                }else {
-                    polygon = new Polygon(newPoint);
+                } else {
+                    let polygon = new Polygon(newPoint); // Cambiado a let
                     drawNewPolygon(polygon);
                 }
-
-                
             });
         });
-
     };
-    
-
 
     return {
-        // evento de clic en el canvas para agregar puntos y 
-        //establecer la conexión WebSocket para recibir puntos de otros usuarios
         init: function () {
             canvas = document.getElementById("canvas");
             canvas.addEventListener("click", function (event) {
-                var mousePosition = getMousePosition(event);
+                let mousePosition = getMousePosition(event); // Cambiado a let
                 app.publishPoint(mousePosition.x, mousePosition.y);
             });
             connectAndSubscribe();
         },
-
-        connect: function (){
-            var canvas = document.getElementById("canvas");
-            var option = document.getElementById("connectionType");
-            var drawId = $("#drawId").val();
-            topic = option.value+drawId;
-            alert("Se a conectado a: "+ drawId);
+        connect: function () {
+            let canvas = document.getElementById("canvas"); // Cambiado a let
+            let option = document.getElementById("connectionType"); // Cambiado a let
+            let drawId = $("#drawId").val(); // Cambiado a let
+            topic = option.value + drawId;
+            alert("Se ha conectado a: " + drawId);
             connectAndSubscribe(topic);
-            if(window.PointerEvent){
-                canvas.addEventListener("pointerdown", function (event){
-                    var point = getMousePosition(event);
-                    //addPointToCanvas(point);
-                    stompClient.send("/topic"+topic, {}, JSON.stringify(point));
+            if (window.PointerEvent) {
+                canvas.addEventListener("pointerdown", function (event) {
+                    let point = getMousePosition(event); // Cambiado a let
+                    stompClient.send("/topic" + topic, {}, JSON.stringify(point));
                 });
             }
         },
-
-        publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            // console.info("publishing point at "+pt);
+        publishPoint: function (px, py) {
+            let pt = new Point(px, py); // Cambiado a let
             addPointToCanvas(pt);
-            stompClient.send("/app"+topic, {}, JSON.stringify(pt));
-            //publicar el evento
+            stompClient.send("/app" + topic, {}, JSON.stringify(pt));
         },
-
         disconnect: function () {
             if (stompClient !== null) {
                 stompClient.disconnect();
             }
-            setConnected(false);
             console.log("Disconnected");
         }
-
-
-        
     };
-
 })();
