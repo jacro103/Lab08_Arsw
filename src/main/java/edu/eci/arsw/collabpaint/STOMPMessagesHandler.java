b2319ef40e989721c5add17654d5841de075c571
unknown <jose.correa-r@labinfo.is.escuelaing.edu.co>
2024-10-24 18:23:49 -0500

    sonarcloud

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -1,6 +1,8 @@
 package edu.eci.arsw.collabpaint;
 
 import edu.eci.arsw.collabpaint.model.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -13,6 +15,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 public class STOMPMessagesHandler {
+    private static final Logger logger = LoggerFactory.getLogger(STOMPMessagesHandler.class);
+    
     private Map<String, CopyOnWriteArrayList<Point>> conex = new ConcurrentHashMap<>();
     private final SimpMessagingTemplate msgt;
 
@@ -24,7 +28,7 @@ public class STOMPMessagesHandler {
     @MessageMapping("/newpoint.{numdibujo}")
     public void handlePointEvent(Point pt, @DestinationVariable String numdibujo) {
         // Registro de la recepción de un nuevo punto
-        System.out.println("Nuevo punto recibido en el servidor: " + pt);
+        logger.info("Nuevo punto recibido en el servidor: {}", pt);
         
         msgt.convertAndSend("/topic/newpoint." + numdibujo, pt);
         
