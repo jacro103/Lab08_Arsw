bfa6720f6cb06c0dc7d390d54ccc6f12ece006a6
unknown <jose.correa-r@labinfo.is.escuelaing.edu.co>
2024-10-24 17:43:33 -0500

    sonarcloud

---
+++ src/main/java/edu/eci/arsw/collabpaint/exception.java
@@ -0,0 +1,11 @@
+package edu.eci.arsw.collabpaint.exception;
+
+public class PointHandlingException extends Exception {
+    public PointHandlingException(String message) {
+        super(message);
+    }
+
+    public PointHandlingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
