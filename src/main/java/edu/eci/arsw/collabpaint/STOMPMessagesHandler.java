336ff62ba63f82230205434e43fce67d2144fccf
unknown <jose.correa-r@labinfo.is.escuelaing.edu.co>
2024-10-24 18:19:49 -0500

    sonarcloud

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -1,40 +1,35 @@
 package edu.eci.arsw.collabpaint;
 
 import edu.eci.arsw.collabpaint.model.Point;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 public class STOMPMessagesHandler {
-    private static final Logger logger = LoggerFactory.getLogger(STOMPMessagesHandler.class);
-    
+
     private Map<String, CopyOnWriteArrayList<Point>> conex = new ConcurrentHashMap<>();
-    private final SimpMessagingTemplate msgt;
 
     @Autowired
-    public STOMPMessagesHandler(SimpMessagingTemplate msgt) {
-        this.msgt = msgt;
-    }
+    SimpMessagingTemplate msgt;
 
     @MessageMapping("/newpoint.{numdibujo}")
-    public void handlePointEvent(Point pt, @DestinationVariable String numdibujo)  {
-        logger.info("Nuevo punto recibido en el servidor: {}", pt); // Reemplaza System.out por logger
-
-        msgt.convertAndSend("/topic/newpoint." + numdibujo, pt);
-        if (conex.get(numdibujo) != null) {
+    public void handlePointEvent(Point pt, @DestinationVariable String numdibujo) throws Exception {
+        System.out.println("Nuevo punto recibido en el servidor!:"+pt);
+        msgt.convertAndSend("/topic/newpoint."+numdibujo, pt);
+        if (conex.get(numdibujo) != null){
             conex.get(numdibujo).add(pt);
-            if (conex.get(numdibujo).size() % 4 == 0) {
+            if (conex.get(numdibujo).size() % 4 == 0){
                 msgt.convertAndSend("/topic/newpolygon." + numdibujo, conex.get(numdibujo));
                 conex.put(numdibujo, new CopyOnWriteArrayList<>());
+
             }
         } else {
             CopyOnWriteArrayList<Point> n = new CopyOnWriteArrayList<>();
@@ -43,4 +38,3 @@ public class STOMPMessagesHandler {
         }
     }
 }
-
