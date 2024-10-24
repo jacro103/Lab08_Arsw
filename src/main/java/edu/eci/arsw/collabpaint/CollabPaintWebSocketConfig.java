420aa33f8cf9dc4abeb964f19b13605ada0a6042
unknown <jose.correa-r@labinfo.is.escuelaing.edu.co>
2024-10-24 17:35:22 -0500

    sonarcloud

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -16,8 +16,14 @@
  */
 package edu.eci.arsw.collabpaint;
 
+/**
+ *
+ * @author hcadavid
+ */
+import java.util.logging.Logger;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -25,6 +31,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -34,5 +41,8 @@ public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerCo
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stompendpoint").withSockJS();
+        
     }
+    
+
 }
